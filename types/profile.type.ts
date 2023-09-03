d394ca4ea51d71287cbd09dedae8ff3e08ba824c
Boclair Temagoua <boclair.temgoua@recrowd.com>
2023-09-03 16:14:12 +0200

    fix(module): fix bug

--- types/profile.type.ts
+++ types/profile.type.ts
@@ -6,8 +6,7 @@ export const arrayColors = [
   { id: "1", name: "gray" },
   { id: "2", name: "indigo" },
   { id: "3", name: "red" },
-  { id: "4", name: "blue" },
-  // { id: "5", name: "green" },
+  // { id: "4", name: "green" },
 ];
 
 export type ProfileFormModel = {
