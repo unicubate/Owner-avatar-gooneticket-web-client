f8ae89b0c52d32ec8b4fe3c73de90fcde5e60f1d
Boclair Temagoua <boclair.temgoua@recrowd.com>
2023-09-06 10:13:40 +0200

    fix(module): fix bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,15 @@ import type { AppProps } from "next/app";
 import { ConfigProvider } from "antd";
 import { NextIntlProvider } from "next-intl";
 import { ThemeProvider } from "next-themes";
-import 'react-quill/dist/quill.snow.css'
+import "react-quill/dist/quill.snow.css";
 import "toastify-js/src/toastify.css";
-import 'animate.css'
+import "animate.css";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  Hydrate,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ContextUserProvider } from "@/components/util/session/context-user";
 const queryClient = new QueryClient();
@@ -15,15 +19,17 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ContextUserProvider>
+      <Hydrate state={pageProps.dehydratedState}>
         <ConfigProvider>
-          <Component {...pageProps} />
+          <ContextUserProvider>
+            <Component {...pageProps} />
 
-          {Boolean(process.env.NEXT_PUBLIC_QUERY_DEV_TOOLS) && (
-            <ReactQueryDevtools initialIsOpen={false} />
-          )}
+            {Boolean(process.env.NEXT_PUBLIC_QUERY_DEV_TOOLS) && (
+              <ReactQueryDevtools initialIsOpen={false} />
+            )}
+          </ContextUserProvider>
         </ConfigProvider>
-      </ContextUserProvider>
+      </Hydrate>
     </QueryClientProvider>
   );
 }
