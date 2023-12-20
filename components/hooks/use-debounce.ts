3fa907cf490da006ce3368122d72dc6901409b96
Boclair Temagoua <boclair.temgoua@recrowd.com>
2023-12-20 14:44:40 +0100

    fix(module): fix import

--- components/hooks/use-debounce.ts
+++ components/hooks/use-debounce.ts
@@ -1,15 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 export const useDebounce = <T>(value: T, delay: number): T => {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
-  useEffect(() => {
+  const debounceHandler = useCallback(() => {
     const handler = setTimeout(() => {
       setDebouncedValue(value);
     }, delay);
+
     return () => {
       clearTimeout(handler);
     };
   }, [value, delay]);
+
+  useEffect(debounceHandler, [debounceHandler]);
+
   return debouncedValue;
 };
--- utils/index.ts
+++ utils/index.ts
@@ -2,6 +2,6 @@ export * from "./alert-notification";
 export * from "./formate-date-dayjs";
 export * from "./paginations";
 export * from "./multiple-utility";
-export * from "./use-debounce";
+export * from "../components/hooks/use-debounce";
 export * from "./end-point";
 export * from "./alert-notification";
