a286ae78e4961cbf63596261459ef1004455ea5f
Boclair Temagoua <boclair.temgoua@recrowd.com>
2024-01-19 13:07:50 +0100

    fi(module): upgrade package

---
+++ components/hooks/use-update-query-string-value-without-navigation.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export const useUpdateQueryStringValueWithoutNavigation = (
+  queryKey: string,
+  queryValue: string
+) => {
+  useEffect(() => {
+    const currentSearchParams = new URLSearchParams(window.location.search);
+    const oldQuery = currentSearchParams.get(queryKey) ?? "";
+    if (queryValue === oldQuery) return;
+
+    if (queryValue) {
+      currentSearchParams.set(queryKey, queryValue);
+    } else {
+      currentSearchParams.delete(queryKey);
+    }
+    const newUrl = [window.location.pathname, currentSearchParams.toString()]
+      .filter(Boolean)
+      .join("?");
+    window.history.replaceState(null, "", newUrl);
+  }, [queryKey, queryValue]);
+};
