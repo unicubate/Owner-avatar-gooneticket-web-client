d617ff19b706e26ebf49768de8849e558bb6d801
Boclair Temagoua <boclair.temgoua@recrowd.com>
2024-02-05 00:29:45 +0100

    fix(module): fix bug component

--- components/layout-dashboard/index.tsx
+++ components/layout-dashboard/index.tsx
@@ -1,7 +1,9 @@
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import Head from 'next/head';
+import { useInputState } from '../hooks';
 import { useAuth } from '../util/context-user';
-import { HeaderHorizontalNavDashboard } from './header-horizontal-nav-dashboard';
-import { HeaderVerticalNavDashboard } from './header-vertical-nav-dashboard';
+import { HorizontalNavDashboard } from './horizontal-nav-dashboard';
+import { VerticalNavDashboard } from './vertical-nav-dashboard';
 
 interface IProps {
   title: string;
@@ -11,6 +13,10 @@ interface IProps {
 const LayoutDashboard = ({ children, title }: IProps) => {
   const { profile, username, theme } = useAuth() as any;
   const user = { profile, username };
+  const { isOpen, setIsOpen } = useInputState();
+  const showDrawer = () => {
+    setIsOpen((i) => !i);
+  };
 
   return (
     <>
@@ -18,25 +24,46 @@ const LayoutDashboard = ({ children, title }: IProps) => {
         <title>
           {title} | {process.env.NEXT_PUBLIC_NAME_SITE}
         </title>
+        <meta
+          property="og:title"
+          content={process.env.NEXT_PUBLIC_NAME_SITE}
+          key="title"
+        />
+        <meta
+          name="description"
+          content="Un Pot is the best way for creators and artists to accept support and membership from their fans."
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
       <div className="flex flex-col">
-        <HeaderHorizontalNavDashboard user={user} />
+        <HorizontalNavDashboard showDrawer={showDrawer} user={user} />
+
+        {/* Fix Drawer */}
+        <Sheet onOpenChange={setIsOpen} open={isOpen} defaultOpen={isOpen}>
+          <SheetTrigger asChild>
+            {/* <Button variant="outline">Open</Button> */}
+          </SheetTrigger>
+          <SheetContent className="dark:border-gray-800 dark:bg-[#1c1b22]">
+            <div className="flex flex-col overflow-y-auto pt-5">
+              <VerticalNavDashboard user={user} />
+            </div>
+          </SheetContent>
+        </Sheet>
+        {/*End Fix Drawer */}
 
         <div className="flex flex-1">
-          {profile?.id ? <HeaderVerticalNavDashboard user={user} /> : null}
+          {profile?.id ? (
+            <div className="hidden md:flex md:w-56 md:flex-col">
+              <div className="fixed flex max-h-screen flex-col pt-5">
+                <VerticalNavDashboard user={user} />
+              </div>
+            </div>
+          ) : null}
 
           <div className={`flex flex-1 flex-col bg-gray-100 dark:bg-[#232325]`}>
             <main>{children}</main>
           </div>
-
-          {/* {profile?.id && theme ?
-            <div className={`flex flex-col flex-1 ${theme === "light" ? "bg-gray-100" : ""}`}>
-              <main>
-                {children}
-              </main>
-            </div>
-            : null} */}
         </div>
       </div>
     </>
--- pages/checkouts/[id]/shop.tsx
+++ pages/checkouts/[id]/shop.tsx
@@ -3,6 +3,7 @@
 import { GetCartsAPI, GetOneCartOrderAPI } from '@/api-site/cart';
 import { GetOneUserPublicAPI } from '@/api-site/user';
 import { ListMiniCats } from '@/components/cart/list-mini-carts';
+import { LayoutCheckoutSite } from '@/components/layout-checkout-site';
 import { CreatePaymentPayPal } from '@/components/payment/create-payment-paypal';
 import { CreatePaymentStripe } from '@/components/payment/stripe/create-payment-stripe';
 import { ButtonInput } from '@/components/ui-setting';
@@ -66,86 +67,51 @@ const CheckoutShop = () => {
   }
   return (
     <>
-      <div className="py-10 bg-gray-50 dark:bg-[#1c1b22] sm:py-16 lg:py-20">
-        <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-          {/* <div className="flex items-center justify-center">
-            <h1 className="text-2xl font-bold text-gray-900">Checkout</h1>
-            <span className="px-2 py-1 ml-4 text-xs font-bold tracking-widest uppercase bg-gray-400 rounded-full rounded-r-nonepy-1 text-gray-50">
-              {' '}
-              3 Items{' '}
-            </span>
-          </div> */}
+      <LayoutCheckoutSite title={`Checkout shop`}>
+        <div className="overflow-hidden bg-white dark:bg-[#121212] shadow rounded-xl">
+          <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="px-5 py-6 dark:bg-[#121212] md:px-8">
+              <div className="flow-root">
+                <ul className="divide-y divide-gray-200 dark:divide-gray-800 -my-7">
+                  {dataTableCarts}
+                </ul>
+              </div>
+            </div>
 
-          <div className="max-w-6xl mx-auto mt-6 md:mt-12">
-            <div className="overflow-hidden bg-white dark:bg-[#121212] shadow rounded-xl">
-              <div className="grid grid-cols-1 md:grid-cols-2">
-                <div className="px-5 py-6 dark:bg-[#121212] md:px-8">
-                  <div className="flow-root">
-                    <ul className="divide-y divide-gray-200 dark:divide-gray-800 -my-7">
-                      {dataTableCarts}
-                    </ul>
-                  </div>
+            <div className="px-5 py-6 md:px-8">
+              <div className="flow-root">
+                <div className="-my-6 divide-y divide-gray-200 dark:divide-gray-800 ">
+                  <div className="py-6">
+                    <div className="mb-2 flex items-center">
+                      <AvatarComponent
+                        size={40}
+                        className="size-10 shrink-0 rounded-full"
+                        profile={user?.profile}
+                      />
 
-                  {/* <hr className="mt-6 divide-gray-200 dark:divide-gray-800" /> */}
-
-                  {/* <form action="#" className="mt-6">
-                    <div className="sm:flex sm:space-x-2.5 md:flex-col lg:flex-row md:space-x-0 lg:space-x-2.5">
-                      <div className="flex-grow">
-                        <label className="sr-only"> Coupon code </label>
-                        <input
-                          type="text"
-                          id=""
-                          name=""
-                          placeholder="Enter coupon code"
-                          className="block w-full px-4 py-3 text-sm font-normal text-gray-900 placeholder-gray-500 bg-white border border-gray-300 rounded-md caret-gray-900 focus:ring-gray-900 focus:border-gray-900"
-                        />
+                      <div className="ml-2 cursor-pointer">
+                        <p className="text-sm font-bold">
+                          {user?.profile?.firstName ?? ''}{' '}
+                          {user?.profile?.lastName ?? ''}
+                        </p>
+                        <p className="mt-1 text-sm font-medium text-gray-500">
+                          Checkout
+                        </p>
                       </div>
-                      <div className="mt-4 sm:mt-0 md:mt-4 lg:mt-0">
-                        <button
-                          type="submit"
-                          className="items-center justify-center w-full px-4 py-3 text-sm font-bold text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-md inline- focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:bg-gray-700"
-                        >
-                          Apply coupon
-                        </button>
+
+                      <div className="ml-auto">
+                        <p className="cursor-pointer text-sm font-medium text-gray-400 transition-all duration-200 hover:text-gray-900">
+                          <Link
+                            className="text-sm font-medium text-blue-600 decoration-2 hover:underline"
+                            href={`/${username}/shop`}
+                          >
+                            Continue Shopping
+                          </Link>
+                        </p>
                       </div>
                     </div>
-                  </form> */}
-                </div>
 
-                <div className="px-5 py-6 md:px-8">
-                  <div className="flow-root">
-                    <div className="-my-6 divide-y divide-gray-200 dark:divide-gray-800 ">
-                      <div className="py-6">
-                        <div className="mb-2 flex items-center">
-                          <AvatarComponent
-                            size={40}
-                            className="size-10 shrink-0 rounded-full"
-                            profile={user?.profile}
-                          />
-
-                          <div className="ml-2 cursor-pointer">
-                            <p className="text-sm font-bold">
-                              {user?.profile?.firstName ?? ''}{' '}
-                              {user?.profile?.lastName ?? ''}
-                            </p>
-                            <p className="mt-1 text-sm font-medium text-gray-500">
-                              Checkout
-                            </p>
-                          </div>
-
-                          <div className="ml-auto">
-                            <p className="cursor-pointer text-sm font-medium text-gray-400 transition-all duration-200 hover:text-gray-900">
-                              <Link
-                                className="text-sm font-medium text-blue-600 decoration-2 hover:underline"
-                                href={`/${username}/shop`}
-                              >
-                                Continue Shopping
-                              </Link>
-                            </p>
-                          </div>
-                        </div>
-
-                        {/* <div className="space-y-5 mt-4">
+                    {/* <div className="space-y-5 mt-4">
                           <>
                             {isCardPay ? (
                               <>
@@ -188,10 +154,10 @@ const CheckoutShop = () => {
                             />
                           </>
                         </div> */}
-                      </div>
-                      <div className="py-6">
-                        <ul className="space-y-3">
-                          {/* <li className="flex items-center justify-between">
+                  </div>
+                  <div className="py-6">
+                    <ul className="space-y-3">
+                      {/* <li className="flex items-center justify-between">
                             <p className="text-sm font-medium dark:text-white">
                               Tva
                             </p>
@@ -209,60 +175,33 @@ const CheckoutShop = () => {
                               </>
                             ) : null}
                           </li> */}
-                          <li className="flex items-center justify-between">
-                            <p className="text-3xl font-medium dark:text-white">
-                              Total
+                      <li className="flex items-center justify-between">
+                        <p className="text-3xl font-medium dark:text-white">
+                          Total
+                        </p>
+                        {newAmount?.value ? (
+                          <>
+                            <p className="ml-auto text-lg font-bold dark:text-white">
+                              {newAmount?.currency}
                             </p>
-                            {newAmount?.value ? (
-                              <>
-                                <p className="ml-auto text-lg font-bold dark:text-white">
-                                  {newAmount?.currency}
-                                </p>
-                                <p className="ml-2 text-lg font-bold dark:text-white">
-                                  {formatePrice({
-                                    value: Number(newAmount?.value),
-                                    isDivide: false,
-                                  }) ?? ''}
-                                </p>
-                              </>
-                            ) : null}
-                          </li>
-                        </ul>
-                      </div>
+                            <p className="ml-2 text-lg font-bold dark:text-white">
+                              {formatePrice({
+                                value: Number(newAmount?.value),
+                                isDivide: false,
+                              }) ?? ''}
+                            </p>
+                          </>
+                        ) : null}
+                      </li>
+                    </ul>
+                  </div>
 
-                      <div className="py-6">
+                  <div className="py-6">
+                    <>
+                      {isCardPay ? (
                         <>
-                          {isCardPay ? (
-                            <>
-                              <CreatePaymentStripe
-                                paymentModel="STRIPE-SHOP"
-                                data={{
-                                  cartOrderId,
-                                  amount: newAmount,
-                                  userReceiveId: cartOrder?.userId,
-                                  userSendId: userVisitor?.id,
-                                  organizationId: cartOrder?.organizationId,
-                                }}
-                              />
-                            </>
-                          ) : (
-                            <>
-                              <div className="mt-2">
-                                <ButtonInput
-                                  onClick={() => setIsCardPay(true)}
-                                  type="button"
-                                  size="lg"
-                                  className="w-full"
-                                  variant="info"
-                                >
-                                  Card Pay
-                                </ButtonInput>
-                              </div>
-                            </>
-                          )}
-
-                          <CreatePaymentPayPal
-                            paymentModel="PAYPAL-SHOP"
+                          <CreatePaymentStripe
+                            paymentModel="STRIPE-SHOP"
                             data={{
                               cartOrderId,
                               amount: newAmount,
@@ -272,33 +211,58 @@ const CheckoutShop = () => {
                             }}
                           />
                         </>
-                      </div>
+                      ) : (
+                        <>
+                          <div className="mt-2">
+                            <ButtonInput
+                              onClick={() => setIsCardPay(true)}
+                              type="button"
+                              size="lg"
+                              className="w-full"
+                              variant="info"
+                            >
+                              Card Pay
+                            </ButtonInput>
+                          </div>
+                        </>
+                      )}
 
-                      <div className="py-6">
-                        <h2 className="font-bold text-gray-500 text-base">
-                          Billing Information
-                        </h2>
-                      </div>
+                      <CreatePaymentPayPal
+                        paymentModel="PAYPAL-SHOP"
+                        data={{
+                          cartOrderId,
+                          amount: newAmount,
+                          userReceiveId: cartOrder?.userId,
+                          userSendId: userVisitor?.id,
+                          organizationId: cartOrder?.organizationId,
+                        }}
+                      />
+                    </>
+                  </div>
 
-                      <div className="py-6">
-                        <h2 className="font-bold text-gray-500 text-base">
-                          Payment Method
-                        </h2>
-                      </div>
-                    </div>
+                  <div className="py-6">
+                    <h2 className="font-bold text-gray-500 text-base">
+                      Billing Information
+                    </h2>
+                  </div>
+
+                  <div className="py-6">
+                    <h2 className="font-bold text-gray-500 text-base">
+                      Payment Method
+                    </h2>
                   </div>
                 </div>
               </div>
             </div>
-
-            <div className="mt-4 text-center">
-              <p className="mt-4 text-sm font-normal text-gray-500">
-                All the taxes will be calculated while checkout
-              </p>
-            </div>
           </div>
         </div>
-      </div>
+
+        <div className="mt-4 text-center">
+          <p className="mt-4 text-sm font-normal text-gray-500">
+            All the taxes will be calculated while checkout
+          </p>
+        </div>
+      </LayoutCheckoutSite>
     </>
   );
 };
