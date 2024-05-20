074c948f3654f8e058f17e09c82354916d017c34
Boclair Temagoua <boclair.temgoua@recrowd.com>
2024-05-20 09:38:20 +0200

    fix(module): fix bug

--- utils/multiple-utility.tsx
+++ utils/multiple-utility.tsx
@@ -5,19 +5,20 @@ import { ImageProfileModel } from '@/types/profile.type';
 interface Props {
   value: number;
   isDivide: boolean;
-  currency: string
+  currency: string;
 }
 
 export const formatePrice = ({ value, isDivide, currency }: Props) => {
   const numberCal = isDivide ? value / 100 : value;
-  const language = initialLang === 'fr' ? 'de' : initialLang
+  const language = ['fr', 'it'].includes(initialLang) ? 'de' : initialLang;
   return (
     <>
-      {currency && numberCal && !isNaN(numberCal) ?
-        `${numberCal.toLocaleString(language.toUpperCase(), {
-          currency: currency,
-          style: 'currency'
-        })}` : null}
+      {currency && numberCal && !isNaN(numberCal)
+        ? `${numberCal.toLocaleString(language.toUpperCase(), {
+            currency: currency,
+            style: 'currency',
+          })}`
+        : null}
     </>
   );
 };
@@ -26,9 +27,9 @@ export const oneImageToURL = (image: ImageProfileModel) => {
   const url =
     image?.key === 'aws'
       ? viewOneFileUploadAPI({
-        folder: 'profiles',
-        fileName: image?.patch,
-      })
+          folder: 'profiles',
+          fileName: image?.patch,
+        })
       : image?.patch;
 
   return url as string;
