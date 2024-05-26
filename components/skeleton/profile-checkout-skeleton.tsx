04bc85f32b20d656dc2ce935d01a08d81b770979
Boclair Temagoua <boclair.temgoua@recrowd.com>
2024-05-26 13:31:51 +0200

    fix(module): fix style

--- components/skeleton/profile-checkout-skeleton.tsx
+++ components/skeleton/profile-checkout-skeleton.tsx
@@ -26,11 +26,11 @@ export const ProfileCheckoutSkeleton = () => {
         </div>
 
         <div className="mt-6 flex items-center">
-          <Skeleton className="h-10 w-36 rounded-md" />
-          <Skeleton className="ml-auto h-10 w-36 rounded-md" />
+          <Skeleton className="h-8 w-36 rounded-md" />
+          <Skeleton className="ml-auto h-8 w-36 rounded-md" />
         </div>
         <div className="mt-6 flex items-center">
-          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-8 w-full" />
         </div>
       </div>
     </>
